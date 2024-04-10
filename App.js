c63106a418f0de5334739942edaeacecdf06afd2
thuyhien10x <thuyhienle10x@gmail.com>
2024-04-10 02:18:11 -0700

    them file

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>It worked</Text>
+      <Text>Meo Meo</Text>
       <StatusBar style="auto" />
     </View>
   );
