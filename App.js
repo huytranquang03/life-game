31407d014e02a0f104703474bd0e61cee2c581d2
Minh <nguyengiangminh99@gmail.com>
2024-04-15 17:43:38 +0700

    xóa mấy file thừa ban đầu

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+// import { View, StyleSheet } from 'react-native';
+// import ProgressBar from './components/ui/GameBar'; // Đường dẫn đến tệp ProgressBar
+// import IconButton from './components/ui/IconButton';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Meo Meo</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   // Hàm xử lý sự kiện khi IconButton được nhấn
+//   const handlePress = () => {
+//     console.log('Icon button pressed!');
+//   };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//   return (
+//     <View style={styles.container}>
+//       {/* Thêm ProgressBar vào giao diện */}
+//       <ProgressBar
+//         progress={50} // Giá trị tiến trình (phần trăm)
+//         color="blue" // Màu sắc của tiến trình
+//         height={10} // Chiều cao của thanh tiến trình
+//         borderRadius={5} // Bo góc cho thanh tiến trình
+//       />
+
+//       {/* Thêm IconButton vào giao diện */}
+//       <IconButton
+//         icon="add" // Biểu tượng Ionicons bạn muốn sử dụng
+//         size={24} // Kích thước biểu tượng
+//         color="blue" // Màu sắc biểu tượng
+//         onPress={handlePress} // Hàm xử lý khi nút được nhấn
+//       />
+
+    
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
