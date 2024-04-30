2db50c25c7b53ed6ac63ff1b52d438d7e9471042
Minh <nguyengiangminh99@gmail.com>
2024-04-30 14:21:33 +0700

    test

--- utils/auth.js
+++ utils/auth.js
@@ -3,17 +3,24 @@ import axios from 'axios';
 const API_KEY = 'AIzaSyD75vS3mMqxfitrGdEBlDDeOw1PcZQonSw';
 
 async function authenticate(mode, email, password) {
-  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  try {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
 
-  const response = await axios.post(url, {
-    email: email,
-    password: password,
-    returnSecureToken: true,
-  });
+    const response = await axios.post(url, {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    });
 
-  const token = response.data.idToken;
+    const token = response.data.idToken;
 
-  return token;
+    return token;
+  } catch (error) {
+    // Xử lý lỗi ở đây
+    // Ví dụ:
+    console.error('Error fetching data:', error);
+    throw new Error('Error fetching data');
+  }
 }
 
 export function createUser(email, password) {
